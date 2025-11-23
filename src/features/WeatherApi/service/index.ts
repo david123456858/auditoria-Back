a88c48608600c6ffcabc5462bf9a68711765dfd9
david123456858 <juandavidperaltafuentes@gmail.com>
2025-11-23 18:51:26 -0500

    Funciona todo falta la api malisiosa

--- src/features/WeatherApi/service/index.ts
+++ src/features/WeatherApi/service/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { FailureProccess, SuccessProcess } from './../../../helpers/result'
 import { crudWeather } from '../../../interfaces/CrudWeather'
@@ -13,6 +15,12 @@ export class serviceWeatherApi implements crudWeather {
       return SuccessProcess(reponseWeather.data, reponseWeather.status)
     } catch (error) {
       console.log(error)
+      if (axios.isAxiosError(error)) {
+        return FailureProccess(
+          error.response?.data?.message || 'Error al consultar clima',
+          error.response?.status || 500
+        )
+      }
       return FailureProccess('Algo paso mi hermano', 500)
     }
   }
